716cea5df74daddbda7419d783590baef4e3f87e
tsheen08 <sheent@oregonstate.edu>
2021-08-13 01:55:20 -0700

    finally

--- microservice/server.js
+++ microservice/server.js
@@ -1,27 +1,35 @@
-var express = require('express');
-const { symlinkSync } = require('fs');
-var http = require('http');
-var app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 var fs = require('fs')
+http = require('http')
+var path = require('path')
+const { nextTick } = require('process')
 
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
-
-
-app.listen(3000, function( ){
-    console.log('server running on port 3000. Ctrl-C to exit');
+app.get('/', (req, res) => {
+    res.send('GET request')
 })
 
-
+app.listen(port, () => {
+    console.log(`listening on at http://localhost:${port}`)
+})
 
 app.post('/', callpyconverter);
 
 function callpyconverter( req, res ) {
     var spawn = require("child_process").spawn;
-    var process = spawn('python', ["./converter.py", req.body.fileName]);
+    var process = spawn('python', ["./converter.py", req.body]);
+
+    var options = {root: path.join(__dirname)}
+    var fileName = './output.tsv'
+  
+    setTimeout(function(){
+        res.sendFile(fileName, options, function(err){
+            if (err){
+                console.log('error:', err)
+            } else {
+                console.log('Sent:', fileName)
+            }
+        })
+    },3000)
 }
-
-// process.stdout.on('data', function (data) {
-//     res.download('./outpust.tsv', 'output.tsv')
-// })
-
